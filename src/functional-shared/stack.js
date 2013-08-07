eb18e7f6d9047a1f0592fd1bca5b8fe843f0ec80
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:49:04 -0700

    Passed push, pop and size test, storage property test, share methods property test

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,41 @@
 var makeStack = function(){
   // Hey! Copy your code from src/functional/stack.js and paste it here
+  var storage = {};
+  var size = 0; // Hint: set an initial value here
+  // Implement the methods below
+  var stack = {
+  };
+  extend(stack, thingy);
+
+  return stack;
+
 };
+
+var extend = function(to, from) {
+    for (var key in from) {
+      to[key] = from[key];
+    }
+};
+
+thingy = {};
+
+thingy.push = function(value){
+    this.size++;
+    this.storage[size] = value;
+  };
+
+thingy.pop = function(){
+    var results = this.storage[this.size];
+    if (this.size > 0) this.size--;
+    return results;
+  };
+
+thingy.size = function(){
+    return this.size;
+  };
+
+var tester = function() {
+  var a = 0;
+  var b = 2;
+};
+tester();
