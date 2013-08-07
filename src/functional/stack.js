f39633e8ccaadc6899fc3692d1aff4719d7af9d4
Hack Reactor Students <github@hackreactor.com>
2013-08-07 13:28:50 -0700

    basic changes for stack

--- src/functional/stack.js
+++ src/functional/stack.js
@@ -2,18 +2,24 @@ var makeStack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   var stack = {};
 
   stack.push = function(value){
+    size++;
+    storage[size] = value;
   };
 
   stack.pop = function(){
+    var toreturn = storage[size];
+    if (size > 0) size--;
+    return toreturn;
   };
 
   stack.size = function(){
+    return size;
   };
 
   return stack;
