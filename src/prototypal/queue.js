6277f7e3a56c0723daf948bb607503984e8d96ca
Hack Reactor Students <github@hackreactor.com>
2013-08-07 19:47:11 -0700

    Created stack and queue using prototypal instantiation

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,31 @@
 var Queue = function() {
   // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  // Implement the methods below
+  var queueobj = Object.create(outfunc);
+  queueobj._storage =  {};
+  queueobj._size = 0;
+  queueobj._created = 0;
+  return queueobj;
 };
+
+outfunc = {};
+
+outfunc.enqueue = function(value){
+    this._size++;
+    this._created++;
+    this._storage[this._created] = value;
+  };
+
+outfunc.dequeue = function(){
+    if (this._size > 0) {
+      this._size--;
+    }
+    var results = this._storage[this._created - this._size];
+    delete this._storage[this._created-this._size];
+    return results;
+  };
+
+outfunc.size = function(){
+    return this._size;
+};
+
--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,34 @@
 var Stack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var stackobj = Object.create(thingy);
+  stackobj['_size'] = 0;
+  stackobj['_storage'] = {};
+  extend(stackobj, thingy);
+
+  return stackobj;
+
 };
+
+var extend = function(to, from) {
+    for (var key in from) {
+      to[key] = from[key];
+    }
+};
+
+thingy = {};
+
+thingy.push = function(value){
+    this._storage[this._size] = value;
+    this._size++;
+  };
+
+thingy.pop = function(){
+    if (this._size > 0) this._size--;
+    var results = this._storage[this._size];
+    return results;
+  };
+
+thingy.size = function(){
+    return this._size;
+  };
+
